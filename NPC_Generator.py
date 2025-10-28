4e06b4330490124f33ebe5c8a9e08b6fe2f3f166
JaydenAngulo <jaydenangulo21@icloud.com>
2025-10-28 18:25:02 +0000

    update

--- NPC_Generator.py
+++ NPC_Generator.py
@@ -1,1 +1,9 @@
-names_list = ["Jayden", "Nick", "Joe", "Kevin", "Gavin", "Bob", "Casper", "Mario", "Michael", "Ryan"]
+names_list = ["Jayden", "Nick", "Joe Schmoe", "Kevin", "Gavin", "Bob", "Casper", "Mario", "Michael", "Ryan"]
+import random
+for names in names_list:
+    strength = int(input(f"Enter the amount of strength you want {names} to have.\nThe strength value must be a whole number between 1 and 100:"))
+    if strength >= 100 and strength <= 1:
+        print("Your answer must be between 1 and 100")
+for names in names_list:
+    speed = random.randint(1, 100)
+    print(f"{names}'s speed is {speed}")
