4badff7b33469fcf3458d8e6fd9e9608bab26881
JaydenAngulo <jaydenangulo21@icloud.com>
2025-10-30 16:27:11 +0000

    update

--- NPC_Generator.py
+++ NPC_Generator.py
@@ -1,9 +1,16 @@
 names_list = ["Jayden", "Nick", "Joe Schmoe", "Kevin", "Gavin", "Bob", "Casper", "Mario", "Michael", "Ryan"]
 import random
+
 for names in names_list:
-    strength = int(input(f"Enter the amount of strength you want {names} to have.\nThe strength value must be a whole number between 1 and 100:"))
-    if strength >= 100 and strength <= 1:
-        print("Your answer must be between 1 and 100")
+    HP = random.randint(0.0, 100.0)
+    print(f"{names}'s HP is {HP}")
+
+for names in names_list:
+    weapon_type_options = ["melee", "sword", "ranged", "magical"]
+    weapon_type = (input(f"Enter a weapon type for {names}. \nYour options are melee, sword, ranged, or magical: "))
+    while weapon_type not in weapon_type_options:
+        weapon_type = input("Your answer must be one of the options: ")
+
 for names in names_list:
     speed = random.randint(1, 100)
     print(f"{names}'s speed is {speed}")
